060cc83fc3a2fcd55ecbb8b539066c66574c5251
Xin Liu <xin.sam.liu@hotmail.com>
2026-05-06 22:05:01 +0900

    feat(ironmlx-p1): RmsNorm + LayerNorm via mlx::fast

--- ironmlx/src/nn/mod.rs
+++ ironmlx/src/nn/mod.rs
@@ -6,12 +6,13 @@
 
 pub mod embedding;
 pub mod linear;
+pub mod norm;
 
 pub use embedding::Embedding;
 pub use linear::Linear;
+pub use norm::{LayerNorm, RmsNorm};
 
 // Added in later P1 tasks:
-// pub mod norm;
 // pub mod mlp;
 // pub mod mrope;
 // pub mod attention;
